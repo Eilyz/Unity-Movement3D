caaaec9f62e5b91f623e1a8cc9a18c67d1b9bab2
Subuthai <39835845+Subuthai@users.noreply.github.com>
2021-12-05 14:19:38 +0300

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -4,7 +4,6 @@ public class CameraFollow : MonoBehaviour {
     private readonly Vector3 _offset = new Vector3(0, 4, -10);
     private GameObject _player;
     private Vector3 _playerPosition;
-
     private void Awake() {
         this._player = GameObject.Find("Player");
     }
